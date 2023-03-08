e50f2e3a0189800843a902542a5a5dd2f7a52bc7
adespan <a.despan@student.tudelft.nl>
2023-03-08 14:45:14 +0100

    Added some more functionality to the Board class.
    Added a field for name, password, getters, setters, constructor and a method to check if the board is password protected.

--- commons/src/main/java/commons/Board.java
+++ commons/src/main/java/commons/Board.java
@@ -1,5 +1,31 @@
 package commons;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Data
+@Entity
 public class Board {
-    
+    @Id
+    @NonNull
+    private String name;
+
+    /* If null the board will not have a password. */
+    private String password;
+
+    // TODO: Implement the list of list of cards.
+
+    /**
+     * Return whether or not the board is password protected.
+     * 
+     * @return a {@link Boolean}
+     */
+    public boolean isPasswordProtected() {
+        return password != null;
+    }
 }
