4411b77ea5cdeaac6c8a83054c5f3cf9db2d742b
Wojciech Graj <w.graj@student.tudelft.nl>
2023-03-08 17:20:57 +0100

    Revert "Merge branch 'entities' into 'main'"
    This reverts merge request !24

--- commons/src/main/java/commons/Board.java
+++ commons/src/main/java/commons/Board.java
@@ -1,28 +1,24 @@
 package commons;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-import lombok.Getter;
+import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Data
 @Entity
 public class Board {
     @Id
-    @Getter
     @NonNull
     private String name;
 
-    @Getter
     /* If null the board will not have a password. */
     private String password;
 
-    private List<CardList> cards = new ArrayList<>();
+    // TODO: Implement the list of list of cards.
 
     /**
      * Return whether or not the board is password protected.
@@ -31,57 +27,5 @@ public class Board {
      */
     public boolean isPasswordProtected() {
         return password != null;
-    }
-
-    /**
-     * Adds an empty {@link CardList} to the board.
-     * 
-     * @return a {@link Boolean} indicating whether or not the {@link CardList} was
-     *         added
-     */
-    public boolean addCardList() {
-        return addCardList(new CardList());
-    }
-
-    /**
-     * Adds a {@link CardList} to the board.
-     * 
-     * @param cardList the {@link CardList} to add
-     * @return a {@link Boolean} indicating whether or not the {@link CardList} was
-     *         added
-     * 
-     */
-    public boolean addCardList(CardList cardList) {
-        if (cardList == null)
-            return false;
-        if (this.cards.contains(cardList))
-            return false;
-        this.cards.add(cardList);
-        return true;
-    }
-
-    /**
-     * Removes a {@link CardList} from the board.
-     * 
-     * @param cardList the {@link CardList} to remove
-     * @return a {@link Boolean} indicating whether or not the {@link CardList} was
-     *         removed
-     */
-    public boolean removeCardList(CardList cardList) {
-        return this.cards.remove(cardList);
-    }
-
-    /**
-     * Removes a {@link CardList} from the board.
-     * 
-     * @param id the id of the {@link CardList} to remove
-     * @return a {@link Boolean} indicating whether or not the {@link CardList} was
-     *         removed
-     */
-    public boolean removeCardList(long id) {
-        CardList cardList = this.cards.stream().filter(c -> c.getId() == id).findFirst().orElse(null);
-        if (cardList == null)
-            return false;
-        return removeCardList(cardList);
     }
 }
