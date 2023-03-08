b72eebc0baa522f13743429aa2e000900ae0f60f
Yusuf Özdemir <y.ozdemir-1@student.tudelft.nl>
2023-03-08 15:05:27 +0100

    Added some methods to cardList

--- commons/src/main/java/commons/CardList.java
+++ commons/src/main/java/commons/CardList.java
@@ -1,9 +1,41 @@
 package commons;
 
-import javax.persistence.Entity;
+import lombok.Data;
 
+import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 @Entity
 public class CardList {
 
+    private List<Card> cardList;
+    private String title;
 
+    /**
+     * Constructor for a card list without a title
+     */
+    public CardList() {
+        this.cardList = new ArrayList<>();
+    }
+    /**
+     * Constructor for a card list with a given title
+     */
+    public CardList(String title) {
+        this.title = title;
+        this.cardList = new ArrayList<>();
+    }
+
+    public List<Card> getCardList() {
+        return cardList;
+    }
+
+    public void removeCard(int index){
+        this.cardList.remove(index);
+    }
+
+    public void addCard(Card card){
+        this.cardList.add(card);
+    }
 }
