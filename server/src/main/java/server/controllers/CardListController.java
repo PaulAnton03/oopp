32f8e6298f8f6ebe6797ebb77335a99c5ddfc757
adespan <a.despan@student.tudelft.nl>
2023-03-11 18:41:53 +0100

    Update CardListController.java

--- server/src/main/java/server/controllers/CardListController.java
+++ server/src/main/java/server/controllers/CardListController.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import commons.CardList;
 import server.database.CardListRepository;
 
+@RestController
+@RequestMapping("/list")
 public class CardListController {
     private CardListRepository cardListRepository;
 
