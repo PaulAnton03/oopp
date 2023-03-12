d386b99ca22a6cab402a3df368e97498c5897468
adespan <a.despan@student.tudelft.nl>
2023-03-12 21:05:52 +0100

    Made connecting to a server of choice work.

--- client/src/main/java/client/scenes/ServerConnectCtrl.java
+++ client/src/main/java/client/scenes/ServerConnectCtrl.java
@@ -2,11 +2,15 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class ServerConnectCtrl {
 
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
+    @FXML
+    private TextField serverInput;
 
     @Inject
     public ServerConnectCtrl(ServerUtils server, MainCtrl mainCtrl) {
@@ -16,17 +20,17 @@ public class ServerConnectCtrl {
     }
 
     public void connect() {
-
+        String server = serverInput.getText();
+        if (server == null || server.isEmpty()) {
+            server = ServerUtils.SERVER;
+        }
         //Connect to server
+        ServerUtils.SERVER = server;
+        System.out.println("Connecting to server: " + server);
 
         mainCtrl.getPrimaryStage().setResizable(true);
         mainCtrl.showMainView();
         //Switch scene
 
-    }
-
-    public void showMainView() {
-        mainCtrl.getPrimaryStage().setResizable(true);
-        mainCtrl.showMainView();
     }
 }
--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -34,7 +34,7 @@ import jakarta.ws.rs.core.GenericType;
 
 public class ServerUtils {
 
-    private static final String SERVER = "http://localhost:8080/";
+    public static String SERVER = "http://localhost:8080/";
 
     public void getQuotesTheHardWay() throws IOException {
         var url = new URL("http://localhost:8080/api/quotes");
