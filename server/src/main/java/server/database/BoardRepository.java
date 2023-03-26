bd5e3fed06ee3b0cfcec1eb44a324754bbc804cc
blytkowski <b.lytkowski@student.tudelft.nl>
2023-03-26 14:28:45 +0200

    Removed redundant delete-down-prop methods
    Signed-off-by: blytkowski <b.lytkowski@student.tudelft.nl>

--- server/src/main/java/server/database/BoardRepository.java
+++ server/src/main/java/server/database/BoardRepository.java
@@ -1,26 +1,10 @@
 package server.database;
 
 import commons.Board;
-import commons.CardList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     Optional<Board> findByName(String name);
-
-    /**
-     * Delete a {@link Board board}, propagating changes down to {@link CardList CardLists}
-     * @param board
-     * @param cardListRepository
-     * @param cardRepository
-     */
-    default void deleteDownProp(Board board, CardListRepository cardListRepository, CardRepository cardRepository) {
-        if (board.getCardLists() != null) {
-            for (CardList cardList: board.getCardLists()) {
-                cardListRepository.deleteDownProp(cardList, cardRepository);
-            }
-        }
-        this.deleteById(board.getId());
-    }
 }
--- server/src/main/java/server/database/CardListRepository.java
+++ server/src/main/java/server/database/CardListRepository.java
@@ -1,21 +1,7 @@
 package server.database;
 
-import commons.Card;
 import commons.CardList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CardListRepository extends JpaRepository<CardList, Long>{
-    /**
-     * Delete a {@link CardList cardList}, only propagating changes down to {@link Card}s
-     * @param cardList
-     * @param cardRepository
-     */
-    default void deleteDownProp(CardList cardList, CardRepository cardRepository) {
-        if (cardList.getCards() != null) {
-            for (Card card: cardList.getCards()) {
-                cardRepository.deleteDownProp(card);
-            }
-        }
-        this.deleteById(cardList.getId());
-    }
+public interface CardListRepository extends JpaRepository<CardList, Long> {
 }
