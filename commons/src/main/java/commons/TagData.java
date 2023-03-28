45d53a5afa4f340cd579dcf6022287f087788f00
Yusuf Özdemir <y.ozdemir-1@student.tudelft.nl>
2023-03-28 14:18:15 +0200

    Update TagData.java

--- commons/src/main/java/commons/TagData.java
+++ commons/src/main/java/commons/TagData.java
@@ -1,8 +1,18 @@
 package commons;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.awt.*;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class TagData {
 
     private Tag tag;
-    private Color
+    private Color color;
+
 
 }
