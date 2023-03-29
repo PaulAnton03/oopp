3dfec3651ca4802a7b79c4daed9801710e43a876
Yusuf Özdemir <y.ozdemir-1@student.tudelft.nl>
2023-03-29 23:29:37 +0200

    Did the method to double click  v easy

--- client/src/main/java/client/components/CardCtrl.java
+++ client/src/main/java/client/components/CardCtrl.java
@@ -236,5 +236,11 @@ public class CardCtrl implements Component<Card>, DBEntityCtrl<Card, Card/* TODO
             client.getActiveCardListCtrl().refresh();
             event.consume();
         });
+
+        cardView.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                editCard();
+            }
+        });
     }
 }
