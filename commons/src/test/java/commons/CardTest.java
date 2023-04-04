379b12ed5420efcb9fcd388ddaae5967c5aa737e
adespan <a.despan@student.tudelft.nl>
2023-04-04 15:20:30 +0200

    Added tests to card.

--- commons/src/test/java/commons/CardTest.java
+++ commons/src/test/java/commons/CardTest.java
@@ -8,6 +8,51 @@ class CardTest {
     Card card = new Card("task", "description");
 
     @Test
+    void hashCodeTest() {
+        Card card2 = new Card("task", "description");
+        assertEquals(card.hashCode(), card2.hashCode());
+
+        card2.setTitle("task2");
+        assertNotEquals(card.hashCode(), card2.hashCode());
+
+        card2.setTitle("task");
+        card2.setDescription("description2");
+        assertNotEquals(card.hashCode(), card2.hashCode());
+
+        card2.setDescription("description");
+        card2.setCardList(new CardList());
+        assertNotEquals(card.hashCode(), card2.hashCode());
+    }
+
+    @Test
+    void equalsTest() {
+        Card card2 = new Card("task", "description");
+        assertTrue(card.equals(card2));
+        assertTrue(card2.equals(card));
+
+        card2.setTitle("task2");
+        assertFalse(card.equals(card2));
+        assertFalse(card2.equals(card));
+
+        card2.setTitle("task");
+        card2.setDescription("description2");
+        assertFalse(card.equals(card2));
+        assertFalse(card2.equals(card));
+
+        card2.setDescription("description");
+        card2.setCardList(new CardList());
+        assertFalse(card.equals(card2));
+        assertFalse(card2.equals(card));
+    }
+
+    @Test
+    void toStringTest() {
+        String cardOutput = card.toString();
+        String expectedOutput = "Card [id=0, title=task, description=description, cardList=null]";
+        assertEquals(expectedOutput, cardOutput);
+    }
+
+    @Test
     void SetGetCardList() {
         CardList list = new CardList();
         card.setCardList(list);
@@ -23,6 +68,8 @@ class CardTest {
     void setTitle() {
         card.setTitle("Title.");
         assertEquals(card.getTitle(), "Title.");
+
+        card.setTitle("task");
     }
 
     @Test
