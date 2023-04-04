a91c3a1cb001b94a32e9bec5b93edca6bb19f8e8
adespan <a.despan@student.tudelft.nl>
2023-04-04 15:20:24 +0200

    Added tests to CardLists.

--- commons/src/test/java/commons/CardListTest.java
+++ commons/src/test/java/commons/CardListTest.java
@@ -10,9 +10,57 @@ class CardListTest {
     Card card = new Card("Do the dishes", "I need to do the dishes before I go out tonight");
 
     @Test
+    void hashCodeTest() {
+        CardList cardList1 = new CardList("MyList");
+        assertEquals(cardList1.hashCode(), cardList.hashCode());
+        cardList1.setTitle("MyList1");
+        assertNotEquals(cardList1.hashCode(), cardList.hashCode());
+        cardList1.setTitle("MyList");
+        cardList1.addCard(card);
+        assertNotEquals(cardList1.hashCode(), cardList.hashCode());
+    }
+
+    @Test
+    void getBoard() {
+        assertNull(cardList.getBoard());
+    }
+
+    @Test
+    void constructorTest() {
+        CardList cardList1 = new CardList("MyList");
+        assertNotNull(cardList1);
+        assertEquals(cardList1.getTitle(), "MyList");
+        assertNotNull(cardList1.getCards());
+
+        cardList1 = new CardList("MyList", "color");
+        assertNotNull(cardList1);
+        assertEquals(cardList1.getTitle(), "MyList");
+        assertNotNull(cardList1.getCards());
+    }
+
+    @Test
+    void setColor() {
+        cardList.setColor("color");
+        assertEquals("color", cardList.getColor());
+
+        cardList = new CardList("MyList");
+    }
+
+    @Test
+    void toStringTest() {
+        String cardListOutput = cardList.toString();
+        String expectedOutput = "CardList [id=0, title=MyList, color=#b2b2ebff, cards=[]]";
+        assertEquals(expectedOutput, cardListOutput);
+    }
+
+    @Test
     void removeCard() {
         cardList.addCard(card);
         cardList.removeCard(card);
+        assertTrue(cardList.getCards().isEmpty());
+
+        cardList.addCard(card);
+        cardList.removeCard(0);
         assertTrue(cardList.getCards().isEmpty());
     }
 
@@ -20,6 +68,10 @@ class CardListTest {
     void addCard() {
         cardList.addCard(card);
         assertFalse(cardList.getCards().isEmpty());
+
+        Card c = new Card();
+        cardList.addCardAtPosition(c, 0);
+        assertSame(c, cardList.getCards().get(0));
     }
 
     @Test
@@ -56,6 +108,13 @@ class CardListTest {
     void testEquals() {
         CardList cardList1 = new CardList("MyList");
         assertEquals(cardList1, cardList);
+
+        cardList1.setTitle("MyList1");
+        assertNotEquals(cardList1, cardList);
+
+        cardList1.setTitle("MyList");
+        cardList1.addCard(card);
+        assertNotEquals(cardList1, cardList);
     }
 
 }
