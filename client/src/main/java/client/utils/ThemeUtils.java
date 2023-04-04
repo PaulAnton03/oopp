3219414005a71db8cf1aeff7230ab7a51fcd1053
adespan <a.despan@student.tudelft.nl>
2023-04-04 17:12:16 +0200

    Added predefined themes.

--- client/src/main/java/client/utils/ThemeUtils.java
+++ client/src/main/java/client/utils/ThemeUtils.java
@@ -3,9 +3,12 @@ package client.utils;
 import javafx.scene.Parent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
 
 @Data
 public class ThemeUtils {
@@ -20,7 +23,7 @@ public class ThemeUtils {
     private Font font;
 
     public void updateFont() {
-        if(font == null) return;
+        if (font == null) return;
         updateStyle(clientUtils.getBoardCtrl().getNode());
         clientUtils.getCardListCtrls().values().forEach(c -> updateStyle(c.getNode()));
         clientUtils.getCardCtrls().values().forEach(c -> updateStyle(c.getNode());
@@ -39,5 +42,21 @@ public class ThemeUtils {
 
     public String turnColorIntoString(Color color) {
         return color.toString().replace("0x", "#");
+    }
+
+    @Data
+    @AllArgsConstructor
+    public class Theme {
+        private String name;
+        private String boardColor;
+        private String cardColor;
+        private String listColor;
+        private Font font;
+    }
+
+    public List<Theme> getPredefinedThemes() {
+        return Arrays.asList(
+                new Theme("Default", "#ffffffff", "#ffffffff", "#b2b2ebff", Font.font("Arial")),
+                new Theme("Dark", "#000000ff", "#000000ff", "#000000ff", Font.font("Arial")));
     }
 }
